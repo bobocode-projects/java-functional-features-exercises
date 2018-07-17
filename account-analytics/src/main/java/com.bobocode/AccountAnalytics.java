9059a3c09f4974b8f0b79fa2afa72aadd358a01f
Taras <tarass.boychuk@gmail.com>
2018-07-17 21:22:41 +0300

    Complete task

--- account-analytics/src/main/java/com.bobocode/AccountAnalytics.java
+++ account-analytics/src/main/java/com.bobocode/AccountAnalytics.java
@@ -1,11 +1,18 @@
 package com.bobocode;
 
 import com.bobocode.model.Account;
+import com.bobocode.model.Sex;
 
 import java.time.Month;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Implement methods using Stream API
@@ -22,19 +29,24 @@ public class AccountAnalytics {
     }
 
     public Account getRichestPerson() {
-        throw new UnsupportedOperationException("It's your job to make it work!"); // todo
+        return accounts.stream()
+                .max(Comparator.comparing(Account::getBalance)).get();
     }
 
     public List<Account> findAccountsByBirthdayMonth(Month birthdayMonth) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); // todo
+        return accounts.stream()
+                .filter(a -> a.getBirthday().getMonth().equals(birthdayMonth))
+                .collect(toList());
     }
 
     public Map<Boolean, List<Account>> partitionMaleAccounts() {
-        throw new UnsupportedOperationException("It's your job to make it work!"); // todo
+        return accounts.stream()
+                .collect(partitioningBy(a->a.getSex().equals(Sex.MALE)));
     }
 
     public Map<String, List<Account>> groupAccountsByEmailDomain() {
-        throw new UnsupportedOperationException("It's your job to make it work!"); // todo
+        return accounts.stream()
+                .collect(groupingBy(a -> a.getEmail().split("@")[1]));
     }
 
 
